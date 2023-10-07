f88519265d48dce98df831624608a6ddce753ece
JoaoPauloClass <theofficialspam8@gmail.com>
2023-10-06 21:59:46 -0300

    'menu-footer-e-comentarios'

--- navbar.js
+++ navbar.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenu = document.querySelector(".mobile-menu");
-    const nav = document.querySelector(".nav-header");
+
+document.addEventListener('DOMContentLoaded', function () { //executar o script apenas depois que a pagina carregar
+    const mobileMenu = document.querySelector(".mobile-menu"); //selecionar o elemento com a classe mobile-menu do HTML
+    const nav = document.querySelector(".nav-header"); //selecionar o elemento com a classe nav-header do HTML
   
-    mobileMenu.addEventListener('click', () => {
-      nav.classList.toggle("active");
+    mobileMenu.addEventListener('click', () => { //a div mobile menu vai executar uma função quando for clicada
+      nav.classList.toggle("active"); //essa função tira se tem e coloca se nao tem a função active, que via CSS faz o menu aparecer e sumir
     });
   });
   
